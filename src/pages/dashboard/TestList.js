fcff71487cf6c32934d387179199c071576c4fb2
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-07-07 17:01:09 -0700

    feat: render each 'form submission' form from forms list

--- src/pages/dashboard/TestList.js
+++ src/pages/dashboard/TestList.js
@@ -1,63 +1,87 @@
 /* eslint-disable */
 import React, { useState, useEffect } from 'react'
 import {
-	Box,
-	IconButton,
-	Paper,
-	Typography,
-	// Typography
+  Box,
+  IconButton,
+  Paper,
+  Typography,
 } from '@material-ui/core'
 import { API, graphqlOperation, Storage } from 'aws-amplify'
 import { listForms } from '../../graphql/queries'
 import { Grid } from '@material-ui/core'
+import Controls from 'src/components/form/controls/_controls';
+import FormSubmission from 'src/components/form/FormSubmission'
 
 const TestList = () => {
-	const [forms, setForms] = useState([]);
+  const [forms, setForms] = useState([]);
+  const [selectedForm, setSelectedForm] = useState(null);
 
-	useEffect(() => {
-		fetchForms();
-	}, [])
+  useEffect(() => {
+    fetchForms();
+  }, [])
 
-	const fetchForms = async () => {
-		try {
-			const formData = await API.graphql(graphqlOperation(listForms));
-			const formList = formData.data.listForms.items;
-			console.log('form list', formList);
-			setForms(formList);
-		} catch (error) {
-			console.log('error on fetching forms', error);
-		}
-	}
-	const idx = 0;
+  const fetchForms = async () => {
+    try {
+      const formData = await API.graphql(graphqlOperation(listForms));
+      const formList = formData.data.listForms.items;
+      console.log('form list', formList);
+      setForms(formList);
+    } catch (error) {
+      console.log('error on fetching forms', error);
+    }
+  }
+  const idx = 0;
 
-	return (
-		<Box
-			width="100%"
-			alignItems="center"
-			justify="center"
-			margin="auto"
-		>
-			{forms.map((form, idx) => {
-				return <Paper variant="outlined" sx={{py: 2, px: 5, m: 1}} key={`form_${idx}`}>
-					<Grid
-						container
-						display="flex"
-						className="formCard"
-						direction="column"
-						alignItems="left"
-						justify="center"
-					>
-						<Grid item xs={12}>
-							<Typography variant="h4" className="formTitle">{form.title}</Typography>
-							<Typography variant="h5" className="formTitle">{form.id}</Typography>
-							<Typography className="formTitle">{form.description}</Typography>
-              <Typography className="formDescription">{form.validations}</Typography>
-						</Grid>
-					</Grid>
-				</Paper>
-			})}
-		</Box>
-	)
+  const handleFormSelection = (form) => {
+    setSelectedForm(form);
+  }
+
+  if (!selectedForm) {
+    return (
+      <Box
+        width="100%"
+        alignItems="center"
+        justify="center"
+        margin="auto"
+      >
+        {
+          forms.map((form, idx) => {
+            return (
+              <Paper
+                variant="outlined"
+                sx={{ py: 2, px: 5, m: 1 }}
+                key={`form_${idx}`}
+              >
+                <Grid
+                  container
+                  display="flex"
+                  className="formCard"
+                  direction="column"
+                  alignItems="left"
+                  justify="center"
+                  onClick={() => handleFormSelection(form)}
+                >
+                  <Grid item xs={12}>
+                    <Typography variant="h4" className="formTitle">{form.title}</Typography>
+                    <Typography variant="h5" className="formTitle">{form.id}</Typography>
+                    <Typography className="formTitle">{form.description}</Typography>
+                    <Typography className="formDescription">{form.validations}</Typography>
+                  </Grid>
+                </Grid>
+              </Paper>
+            )
+          })
+        }
+      </Box>
+    )
+  } else {
+    return (
+      <>
+        <Controls.Button text="Return to list" color="secondary" fullWidth onClick={() => handleFormSelection(null)} />
+        <FormSubmission formDesign={selectedForm} />
+      </>
+    )
+  }
 }
 
 export default TestList;
