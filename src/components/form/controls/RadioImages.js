a34a4669c225ba380606a780e0a9bcebcf51c7bb
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-08-09 17:34:46 -0700

    update: direct src to getFileAccessUrl of option's image in S3

--- src/components/form/controls/RadioImages.js
+++ src/components/form/controls/RadioImages.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Storage } from 'aws-amplify';
 import {
   Box,
   FormControl,
@@ -34,6 +35,21 @@ const RadioImages = ({
     // config.helperText = meta.error;
   }
 
+  // Get the complete access-URL for the image; appends the following base
+  // https://form-content33808-feat.s3.us-west-2.amazonaws.com/public/...
+  const getFileAccessUrl = async (imageFilePath) => {
+    try {
+      const fileAccessUrl = await Storage.get(imageFilePath, { expires: 60 });
+      // console.log('fileAccessUrl', fileAccessUrl);
+      // Storage.list('')
+      //     .then(result => console.log("Storage List", result))
+      //     .catch(err => console.log("Storage List Error", err))
+      return fileAccessUrl;
+    } catch (error) {
+      console.error('error accessing the file from S3', error);
+    }
+  }
+
   return (
     <div className="form-group">
       <FormControl component="fieldset">
@@ -44,15 +60,21 @@ const RadioImages = ({
             const id = `${option.id}opt${index + 1}`;
             return (
               <Box display="column" align="center" mb={2}>
-                <img src="https://source.unsplash.com/random" alt="test-image" width="100%" />
+                {/* <img src="https://source.unsplash.com/random" alt="test-image" width="100%" /> */}
+                <img
+                  src={getFileAccessUrl(option)}
+                  alt="test-image"
+                  width="100%"
+                />
                 <FormControlLabel
                   key={id}
                   value={option}
                   control={<Radio />}
-                  label={option}
+                  // label={option}
+                  label={''}
                 >
                 </FormControlLabel>
-              </Box>  
+              </Box>
             )
           })}
         </MuiRadioGroup>
