2132a8dc8ed0fb4f2f101a0dcbb260827dfbcf5a
Jesse White <jesse.white6@gmail.com>
2021-09-07 09:40:32 -0700

    feat: add tags to state and complete functionality for tag filter.
    76067607+MicahOlson@users.noreply.github.com

--- src/components/search/SearchMain.js
+++ src/components/search/SearchMain.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { Grid, Link, Chip } from '@material-ui/core';
+import { Grid, Link, Chip, Card, Box } from '@material-ui/core';
 import { listForms, listFormSubmissions } from 'src/graphql/queries';
 import { API, graphqlOperation } from 'aws-amplify';
 import SearchForms from './SearchForms';
@@ -10,9 +10,16 @@ import { TAGS } from '../dashboard/forms/FormConstants.js';
 const SearchMain = () => {
   const [forms, setForms] = useState([]);
 
+  const [filteredForms, setFilteredForms] = useState([]);
+
+  const [availableTags, setAvailableTags] = useState([]);
+
+  const [tagsToFilter, setTagsToFilter] = useState([]);
+
   useEffect(() => {
     fetchForms();
     fetchSubmission();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   const fetchForms = async () => {
@@ -21,6 +28,8 @@ const SearchMain = () => {
       const formList = formData.data.listForms.items;
       // console.log('form list', formList);
       setForms(formList);
+      setFilteredForms(formList);
+      console.log("forms", forms);
     } catch (error) {
       console.log('error on fetching forms', error);
     }
@@ -35,85 +44,163 @@ const SearchMain = () => {
       const subInfo = subData.data.listFormSubmissions.items;
       setSubmissions(subInfo);
       // setForms(subInfo);
-      console.log("submission information:", subInfo);
+      // console.log("submission information:", subInfo);
     } catch (error) {
       console.log('error on fetching submission', error);
     }
   };
 
-  const [availableTags, setAvailableTags] = useState([]);
-
-  const [tagsToFilter, setTagsToFilter] = useState([]);
-
+  //constantly checking what is in filteredTags and removing it from availableTags when filteredTags is updated
   useEffect(() => {
     const postFilteredTags = TAGS.filter(tag => !tagsToFilter.includes(tag));
-    setAvailableTags(postFilteredTags)
+    setAvailableTags(postFilteredTags);
+    // const taggedForms = filteredForms.filter(form => form.tags.includes(...tagsToFilter));
+    // const taggedForms = getTaggedForms();
+    const taggedForms = updateFilteredForms();
+    console.log("taggedForms", taggedForms);
+    console.log("tagsToFilter", tagsToFilter)
+    setFilteredForms(taggedForms);
+    console.log("filteredForms", filteredForms)
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [tagsToFilter])
 
+  // useEffect(() => {
+  //   for (let i = 0; i < forms.length; i++) {
+  //     for (let j = 0; j < tagsToFilter.length; j++) {
+  //       if (forms[i].tags.includes(j)) {
+  //         setFilteredForms(forms[i])
+  //         console.log("forms useEffect Statement", forms[i])
+  //       } else {
+  //         console.log("forms useEffect did not run")
+  //       }
+  //     }
+  //   }
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [availableTags])
+
+  // useEffect(() => {
+  //   for (let i = 0; i < forms.length; i++) {
+  //     for (let j = 0; j < forms[i].tags.length; j++) {
+  //       if (tagsToFilter.includes(forms[i].tags[j])) {
+  //         setFilteredForms(forms[i])
+  //       } else {
+  //         console.log("setFilterForms loop did not run")
+  //       }
+  //     }
+  //   }
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [tagsToFilter])
+
+  // const getTaggedForms = () => {
+  //   let filteredFormsArray = [];
+  //   tagsToFilter.forEach(tag => {
+  //     const taggedForms = filteredForms.filter(form => form.tags.includes(tag));
+  //     filteredFormsArray.push(...taggedForms);
+  //   });
+  //   const setFormsArray = new Set(filteredFormsArray)
+  //   console.log(setFormsArray)
+  //   console.log(Array.from(setFormsArray))
+  //   return Array.from(setFormsArray);
+  // }
+
+  // const getTaggedForms = () => {
+  //   const taggedForms = filteredForms.filter(form => form.tags.includes(tagsToFilter[tagsToFilter.length - 1]));
+  //   return taggedForms;
+  // };
+
+  const updateFilteredForms = () => {
+    const filtered = forms.filter(form => tagsToFilter.every(tag => form.tags.includes(tag)));
+    return filtered;
+  }
+
   const addTag = (tag) => {
-    // const tagToAdd = availableTags[tagidx];
-    const filteredForms = forms.filter(form => form.tags.includes(tag));
-    setTagsToFilter([...tagsToFilter, tag])
-    setForms(filteredForms);
+    setTagsToFilter([...tagsToFilter, tag]);
+    console.log("tags", availableTags)
+    // const taggedForms = filteredForms.filter(form => form.tags.includes(...tagsToFilter));
+    // console.log("taggedForms", taggedForms)
+    // setFilteredForms(taggedForms)
+    // for (let i = 0; i < forms.length; i++) {
+    //   for (let j = 0; j < forms[i].tags.length; j++) {
+    //     if (tagsToFilter.includes(forms[i].tags[j])) {
+    //       setFilteredForms(forms[i])
+    //     } else {
+    //       console.log("setFilteredForms loop did not run")
+    //     }
+    //   }
+    // }
   };
 
   // Remove tag from mapped array
-
   const removeTag = (tag) => {
-    const filteredForms = forms.filter(form => form.tags.includes(tag));
+    // const unTaggedForms = filteredForms.filter(form => !form.tags.includes(tag));
     const newTags = tagsToFilter.filter(e => e !== tag);
-    setForms(filteredForms)
     setTagsToFilter(newTags)
+    // setFilteredForms(forms)
+    console.log("newTag", newTags)
+    console.log("filteredForms", filteredForms)
+    // console.log("unTaggedForms", unTaggedForms)
+    console.log("forms", forms)
   };
 
   return (
     <>
-      <Grid container justifyContent="center">
-        {
-          availableTags.map(tag => {
-            return (
-              <Chip
-                label={tag}
-                type="text"
-                id={tag}
-                className={tag}
-                value={tag}
-                sx={{
-                  m: 1,
-                  mb: 5,
-                  p: 2,
-                }}
-                color="secondary"
-                clickable
-                onClick={() => addTag(tag)}
-              />
-            )
-          })
-        }
-        {
-          tagsToFilter.map(tag => {
-            return (
-              <Chip
-                label={tag}
-                type="text"
-                id={tag}
-                className={tag}
-                value={tag}
-                sx={{
-                  m: 1,
-                  mb: 5,
-                  p: 2,
-                  backgroundColor: 'blue'
-                }}
-                onDelete={() => removeTag(tag)}
-              />
-            )
-          })
-        }
-      </Grid>
+      <Box>
+        <Card
+          display="flex"
+          justifyContent="center"
+          sx={{
+            mb: 5,
+            pt: 3,
+            width: "100%",
+          }}>
+          {
+            availableTags.map(tag => {
+              return (
+                <Chip
+                  label={tag}
+                  type="text"
+                  id={tag}
+                  className={tag}
+                  value={tag}
+                  sx={{
+                    ml: 5,
+                    mb: 3,
+                    p: 2,
+                    width: 100
+                  }}
+                  variant="outlined"
+                  color="primary"
+                  clickable
+                  onClick={() => addTag(tag)}
+                />
+              )
+            })
+          }
+          {
+            tagsToFilter.map(tag => {
+              return (
+                <Chip
+                  label={tag}
+                  type="text"
+                  id={tag}
+                  className={tag}
+                  value={tag}
+                  sx={{
+                    ml: 5,
+                    mb: 3,
+                    p: 2,
+                  }}
+                  color="primary"
+                  clickable
+                  onDelete={() => removeTag(tag)}
+                />
+              )
+            })
+          }
+        </Card>
+      </Box>
       <Grid container spacing={2} xs={12}>
-        {forms.map((form, index) => {
+        {filteredForms.map((form, index) => {
           if (!form.isPrivate) {
             return (
               <Grid
