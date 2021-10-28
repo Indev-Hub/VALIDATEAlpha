e97a97464503cd4f3e0101f2a113b762f107cdeb
indev-lee <lee@indevhub.com>
2021-10-27 23:37:46 -0700

    update: make images fit their container in the submission form, use a presigned url to get the images from S3 instead of the AmplifyS3Image component, display image options using a grid format

--- src/components/form/controls/RadioImages.js
+++ src/components/form/controls/RadioImages.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { AmplifyS3Image } from "@aws-amplify/ui-react";
 import {
   Box,
+  Grid,
   FormControl,
   FormControlLabel,
   FormHelperText,
@@ -11,6 +12,26 @@ import {
   Typography
 } from '@material-ui/core';
 import { useField, useFormikContext } from 'formik';
+
+// Use aws-sdk
+const AWS = require('aws-sdk')
+
+// Set which bucket to pull images from
+const myBucket = process.env.REACT_APP_S3_BUCKET
+
+// The myKey variable is not used here because we get the key dynamically in the .map below
+// const myKey = 'FILE_NAME.JPG' 
+
+// Set the time limit for how long the signed url will be good for
+const signedUrlExpireSeconds = 60 * 1
+
+// Set up function to retrieve images from S3 (used in .map below)
+const s3 = new AWS.S3({
+  accessKeyId: process.env.REACT_APP_ACCESS_ID,
+  signatureVersion: 'v4',
+  region: process.env.REACT_APP_REGION,
+  secretAccessKey: process.env.REACT_APP_ACCESS_KEY
+});
 
 const RadioImages = ({
   name,
@@ -40,20 +61,46 @@ const RadioImages = ({
         {/* <FormLabel component="legend">{other.label}</FormLabel> */}
         <Typography>{other.altlabel}</Typography>
         <MuiRadioGroup {...config}>
-          {options.map((option, index) => {
-            const id = `${option.id}opt${index + 1}`;
-            return (
-              <Box display="column" align="center" mb={2}>
-                <AmplifyS3Image imgKey={option} />
-                <FormControlLabel
-                  key={id}
-                  value={option}
-                  control={<Radio />}
+          {/* Set up grid so we can define how the images are displayed based on screen width */}
+          <Grid
+            container
+            spacing={3}
+            mt={2}
+          >
+            {options.map((option, index) => {
+              const id = `${option.id}opt${index + 1}`;
+              const optionURL = s3.getSignedUrl('getObject', {
+                Bucket: myBucket,
+                Key: option,
+                Expires: signedUrlExpireSeconds
+              })
+          
+              console.log(optionURL)
+              return (
+                // <Box display="column" align="left" mb={2}>
+                // Switch from Box to Grid to allow for more than one image to be displayed on each row
+                // Switch out AmplifyS3Image with optionURL variable
+                <Grid
+                  item
+                  align="center"
+                  xs={6}
                 >
-                </FormControlLabel>
-              </Box>
-            )
-          })}
+                  {/* <Box width="500px"> */}
+                    <img src={optionURL} width="100%" />
+                  {/* </Box> */}
+                  {/* <AmplifyS3Image imgKey={option} /> */}
+                  {console.log('option', option)}
+                  <FormControlLabel
+                    key={id}
+                    value={option}
+                    control={<Radio />}
+                  >
+                  </FormControlLabel>
+                </Grid>
+                // </Box>
+              )
+            })}
+          </Grid>
         </MuiRadioGroup>
       </FormControl>
       {meta.touched && meta.error ? (
