46a476e1c7e2b3356cb24c6cc4afed5aca9a5942
Cat-Denton <willwdenton@gmail.com>
2021-11-03 17:06:43 -0700

    feature: update sidebar so company or add company show conditionally on whether a user has companies

--- src/components/dashboard/DashboardSidebar.js
+++ src/components/dashboard/DashboardSidebar.js
@@ -5,7 +5,6 @@ import PropTypes from 'prop-types';
 import {
   Avatar,
   Box,
-  // Button,
   Divider,
   Drawer,
   Hidden,
@@ -13,32 +12,12 @@ import {
   Typography
 } from '@material-ui/core';
 import useAuth from '../../hooks/useAuth';
-// import ReceiptIcon from '@material-ui/icons/Receipt';
-// import BriefcaseIcon from '../../icons/Briefcase';
-// import CalendarIcon from '../../icons/Calendar';
-// import ChartPieIcon from '../../icons/ChartPie';
-// import ChartSquareBarIcon from '../../icons/ChartSquareBar';
-// import ChatAltIcon from '../../icons/ChatAlt';
-// import ClipboardListIcon from '../../icons/ClipboardList';
-// import FolderOpenIcon from '../../icons/FolderOpen';
-// import MailIcon from '../../icons/Mail';
-// import ShareIcon from '../../icons/Share';
-// import ShoppingBagIcon from '../../icons/ShoppingBag';
-// import ShoppingCartIcon from '../../icons/ShoppingCart';
-import UserIcon from '../../icons/User';
-// import UsersIcon from '../../icons/Users';
-import {
-  Archive,
-  Clipboard,
-  ClipboardList,
-  IndevLogo1,
-  Plus
-} from '../../icons';
+import { Plus } from '../../icons';
 import Logo from '../Logo';
 import NavSection from '../NavSection';
 import Scrollbar from '../Scrollbar';
-import { ConsoleLogger } from '@aws-amplify/core';
 import { getUser } from 'src/graphql/queries';
+
 
 const DashboardSidebar = (props) => {
   const { onMobileClose, openMobile } = props;
@@ -77,230 +56,44 @@ const DashboardSidebar = (props) => {
           {
             title: 'Overview',
             path: '/dashboard',
-            // icon: <IndevLogo1 fontSize="small" />
           },
-          // {
-          //   title: 'Analytics',
-          //   path: '/dashboard/analytics',
-          //   icon: <ChartPieIcon fontSize="small" />
-          // },
-          // {
-          //   title: 'Finance',
-          //   path: '/dashboard/finance',
-          //   icon: <ShoppingBagIcon fontSize="small" />
-          // },
           {
             title: 'Account',
             path: '/dashboard/account',
-            // icon: <UserIcon fontSize="small" />
           },
           {
             title: 'Profile',
             path: '/dashboard/profile',
-            // icon: <UserIcon fontSize="small" />
           }
         ]
       },
-      // {
-      //   title: 'VALIDATE Tests',
-      //   items: [
-      //     {
-      //       title: 'Customers',
-      //       path: '/dashboard/customers',
-      //       icon: <UsersIcon fontSize="small" />,
-      //       children: [
-      //         {
-      //           title: 'List',
-      //           path: '/dashboard/customers'
-      //         },
-      //         {
-      //           title: 'Details',
-      //           path: '/dashboard/customers/1'
-      //         },
-      //         {
-      //           title: 'Edit',
-      //           path: '/dashboard/customers/1/edit'
-      //         }
-      //       ]
-      //     },
-      //     {
-      //       title: 'Products',
-      //       path: '/dashboard/products',
-      //       icon: <ShoppingCartIcon fontSize="small" />,
-      //       children: [
-      //         {
-      //           title: 'List',
-      //           path: '/dashboard/products'
-      //         },
-      //         {
-      //           title: 'Create',
-      //           path: '/dashboard/products/new'
-      //         }
-      //       ]
-      //     },
-      //     {
-      //       title: 'Add Test',
-      //       path: '/dashboard/test-create',
-      //       icon: <Plus fontSize="small" />
-      //     },
-      //     {
-      //       title: 'Test Collection',
-      //       path: '/dashboard/test-list',
-      //       icon: <Archive fontSize="small" />
-      //     }
-      //   ]
-      // },
-      // {
-      //   title: 'VALIDATE Analytics',
-      //   icon: <Plus fontSize="small" />,
-      //   path: '/dashboard/orders',
-      //   items: [
-      //     {
-      //       title: 'Add Test',
-      //       path: '/dashboard/test-create',
-      //       icon: <Plus fontSize="small" />
-      //     },
-      //     {
-      //       title: 'Test Collection',
-      //       path: '/dashboard/test-list',
-      //       icon: <Archive fontSize="small" />
-      //     }
-      //   ]
-      // },
       {
         title: 'Company',
         items: [
           {
-            title: companies.length > 0 ? 'Company' : '+ Add Company',
-            // title: 'Company',
-            path: companies.length > 0 ? '/dashboard/company' : '/dashboard/company/new'
-            // icon: <Plus fontSize="small" />
+            title: companies.length > 0 ? 'Company' : 'Add Company',
+            path: companies.length > 0 ? '/dashboard/company' : '/dashboard/company/new',
+            icon: companies.length > 0 ? null : <Plus fontSize="small" />
           },
-          // {
-          //   title: 'Add Company',
-          //   path: '/dashboard/company/new',
-          //   icon: <Plus fontSize="small" />
-          // },
           {
             title: 'Form Collection',
             path: '/dashboard/company/forms',
-            // icon: <Archive fontSize="small" />
           },
           {
             title: 'Add Form',
-            path: '/dashboard/test-create',
+            path: '/dashboard/form-create',
             icon: <Plus fontSize="small" />
           },
-    
-          // {
-          //   title: 'Validations',
-          //   // icon: <Clipboard fontSize="small" />,
-          //   path: '/dashboard/orders',
-          //   children: [
-          //     {
-          //       title: 'Add Form',
-          //       path: '/dashboard/test-create',
-          //       icon: <Plus fontSize="small" />
-          //     },
-          //     {
-          //       title: 'Form Collection',
-          //       path: '/dashboard/form-collection',
-          //       // icon: <Archive fontSize="small" />
-          //     }
-          //   ]
-          // }
         ]
       },
       {
         items: [
           {
             title: 'ANALYTICS',
-            // icon: <ClipboardList fontSize="small" />,
             path: '/dashboard/validation'
           }
         ]
       }
-      //     {
-      //       title: 'Invoices',
-      //       path: '/dashboard/invoices',
-      //       icon: <ReceiptIcon fontSize="small" />,
-      //       children: [
-      //         {
-      //           title: 'List',
-      //           path: '/dashboard/invoices'
-      //         },
-      //         {
-      //           title: 'Details',
-      //           path: '/dashboard/invoices/1'
-      //         }
-      //       ]
-      //     }
-      //   ]
-      // },
-      // {
-      //   title: 'Platforms',
-      //   items: [
-      //     {
-      //       title: 'Projects',
-      //       path: '/dashboard/projects',
-      //       icon: <BriefcaseIcon fontSize="small" />,
-      //       children: [
-      //         {
-      //           title: 'Browse',
-      //           path: '/dashboard/projects/browse'
-      //         },
-      //         {
-      //           title: 'Details',
-      //           path: '/dashboard/projects/1'
-      //         },
-      //         {
-      //           title: 'Create',
-      //           path: '/dashboard/projects/new'
-      //         }
-      //       ]
-      //     },
-      //     {
-      //       title: 'Social',
-      //       path: '/dashboard/social',
-      //       icon: <ShareIcon fontSize="small" />,
-      //       children: [
-      //         {
-      //           title: 'Profile',
-      //           path: '/dashboard/social/profile'
-      //         },
-      //         {
-      //           title: 'Feed',
-      //           path: '/dashboard/social/feed'
-      //         }
-      //       ]
-      //     }
-      //   ]
-      // },
-      // {
-      //   title: 'Apps',
-      //   items: [
-      //     {
-      //       title: 'Kanban',
-      //       path: '/dashboard/kanban',
-      //       icon: <ClipboardListIcon fontSize="small" />
-      //     },
-      //     {
-      //       title: 'Mail',
-      //       path: '/dashboard/mail',
-      //       icon: <MailIcon fontSize="small" />
-      //     },
-      //     {
-      //       title: 'Chat',
-      //       path: '/dashboard/chat',
-      //       icon: <ChatAltIcon fontSize="small" />
-      //     },
-      //     {
-      //       title: 'Calendar',
-      //       path: '/dashboard/calendar',
-      //       icon: <CalendarIcon fontSize="small" />
-      //     }
-      //   ]
-      // }
     ];
   }
 
@@ -398,30 +191,6 @@ const DashboardSidebar = (props) => {
           ))}
         </Box>
         <Divider />
-        {/* <Box sx={{ p: 2 }}>
-          <Typography
-            color="textPrimary"
-            variant="subtitle2"
-          >
-            Need Help?
-          </Typography>
-          <Typography
-            color="textSecondary"
-            variant="body2"
-          >
-            Check our docs
-          </Typography>
-          <Button
-            color="primary"
-            component={RouterLink}
-            fullWidth
-            sx={{ mt: 2 }}
-            to="/docs"
-            variant="contained"
-          >
-            Documentation
-          </Button>
-        </Box> */}
       </Scrollbar>
     </Box>
   );
